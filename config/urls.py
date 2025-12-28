f41c5a85b24f7c82e886fcbb2a9eb70029b23af2
Ghost <sotharathc@gmail.com>
2025-12-28 09:56:40 +0330

    add Js task toggle with fetch endpoint

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from tasks.urls import urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', include('tasks.urls')),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import JsonResponse
+from .models import Task
 
-# Create your views here.
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, "tasks/task_list.html", {"tasks": tasks})
+
+def toggle_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return JsonResponse({"completed": task.completed})
