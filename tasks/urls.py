1d6032def7a728161809b8618d92ea13f81d0314
Ghost <sotharathc@gmail.com>
2025-12-31 13:39:40 +0330

    WIP

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import task_list, toggle_task
+from .views import task_list, toggle_task, delete_task
 
 urlpatterns = [
     path("", task_list, name="task-list"),
     path("toggle/<int:task_id>/", toggle_task, name="toggle-task"),
+    path("delete/<int:task_id>/", delete_task, name="delete-task"),
 ]
